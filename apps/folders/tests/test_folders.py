10c1648f29985e8709738bd0f65390e0606cdbc3
72hrs <jtumpa@gmail.com>
2025-08-15 20:07:01 +0200

    chore: remove old tests

--- apps/folders/tests/test_folders.py
+++ apps/folders/tests/test_folders.py
@@ -25,27 +25,6 @@ def test_select(client, folder_data):
     assert response.status_code == 302
 
 
-def test_insert(client):
-    data = {
-        "app": "agenda",
-        "name": "More Tasks",
-    }
-    response = client.post("/folders/insert/", data)
-    assert response.status_code == 200
-    found = Folder.objects.filter(name="More Tasks").exists()
-    assert found
-
-
-def test_update(client, folder_data):
-    data = {
-        "name": "Better Tasks",
-    }
-    response = client.post(f"/folders/update/{folder_data.id}", data)
-    assert response.status_code == 200
-    found = Folder.objects.filter(name="Better Tasks").exists()
-    assert found
-
-
 def test_delete(client, folder_data):
     client.get(f"/folders/delete/{folder_data.id}")
     found = Folder.objects.filter(id=folder_data.id).exists()
